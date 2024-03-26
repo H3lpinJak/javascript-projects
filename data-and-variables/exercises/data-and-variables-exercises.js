e19e647d10f21b37a7515b2ac1d9e6e5f55c36c9
Ricky <ricky@Luiss-MacBook-Air.local>
2024-03-26 13:44:37 -0500

    data-and-variables-exercises.js

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -1,11 +1,16 @@
-// Declare and assign the variables below
+let shuttlename = 'Determination';
+let ShuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 384400;
+const MilesPerKm = 0.621;
 
-// Use console.log to print the 'typeof' each variable. Print one item per line.
+console.log(typeof shuttlename);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof MilesPerKm)
 
-// Calculate a space mission below
-
-// Print the results of the space mission calculations below
-
-// Calculate a trip to the moon below
-
-// Print the results of the trip to the moon below
+let milesToMars = distanceToMarsKm * MilesPerKm;
+let hoursToMars = distanceToMarsKm / ShuttleSpeedMph;
+let daysToMars = 12857.1428 / 24;
+"Determination will take 535.714 days to reach Mars."
+console.log(shuttlename + " will take " + daysToMars + " days to reach the moon.");
